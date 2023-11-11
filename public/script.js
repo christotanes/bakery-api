6a18efd724cf92ab7715b1dd0e724cf39c67b5c4
christopher.totanes <christopher.totanes@gmail.com>
2023-11-11 18:25:55 +0800

    Refactor index.ejs

--- public/script.js
+++ public/script.js
@@ -1,18 +1,2 @@
 console.log("Hello World!");
-fetch('./login', {
-    method: 'POST',
-    // ... other fetch options
-})
-.then(response => response.json())
-.then(data => {
-    // Handle the JSON data
-    if (data.redirect) {
-        window.location.href = data.redirect; // Redirect the user
-    } else {
-        
-        // Handle other response data if needed
-    }
-})
-.catch(error => {
-    console.error('Error during registration:', error);
-});
+
